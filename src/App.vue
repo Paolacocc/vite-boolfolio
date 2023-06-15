575fa21626f891edd9c78c1e387ad7a84eee2f57
Paolacocc <coccettipaola@gmail.com>
2023-06-15 14:58:00 +0200

    Add homePage and ProjectsPage

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <script>
-
-import axios from 'axios';
-
 export default {
-  data() {
-    return {
-      projects: []
-
-    };
-  },
-  mounted() {
-    this.getProject();
-  },
-  methods: {
-    getProject() {
-      axios.get('http://127.0.0.1:8007/api/projects').then(resp=> {
-        this.projects = resp.data.results;
-      })
-    }
   
-  }
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="row row-col-3">
-      <div class="col" v-for="project in projects" :key="'project.id'">
-      <div>
-        <h5>{{ project.title }}</h5>
-      </div> 
-    </div>
-    </div>
-  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
---
+++ src/pages/ProjectsPage.vue
@@ -0,0 +1,46 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "ProjectsPage",
+
+
+    data() {
+        return {
+            projects: []
+
+        };
+    },
+    mounted() {
+        this.getProject();
+    },
+    methods: {
+        getProject() {
+            axios.get('http://127.0.0.1:8007/api/projects').then(resp => {
+                this.projects = resp.data.results;
+            })
+        }
+
+    }
+}
+</script>
+
+<template>
+<div class="bk-main">
+
+    <div class="container">
+      <h2 class="text-center m-4">Projects list</h2>
+
+      <div class="row row-col-4 g-4">
+        <div class="col" v-for="project in projects" :key="'project.id'">
+        <div class="card h-100 p-3">
+          <h5>{{ project.title }}</h5>
+        </div> 
+      </div>
+      </div>
+    </div>
+  </div>
+
+  </template>
+
+<style lang="scss" scoped></style>
